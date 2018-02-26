f86dfd8b756429e8b183b0811dcba8f11d224330
Asher Cohen <asherlc@asherlc.com>
2018-02-26 12:24:47 -0800

    Remove style rules

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('eslint-plugin-import')
+require('eslint-plugin-jest')
 
 module.exports = {
     extends: [
@@ -8,9 +9,8 @@ module.exports = {
     rules: {
         "import/no-named-as-default-member": 0,
         "no-console": 0,
-        "arrow-body-style": "off",
-        'one-var': 'off',
         'class-methods-use-this': 'warn',
+        'react/no-multi-comp': 'off',
         'no-unused-expressions': ["error", {
             "allowTernary": true
         }],
